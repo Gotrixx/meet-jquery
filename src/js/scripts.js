719ac4f022c9eafb3a28522160f6a23ac47da75a
Christophe Remacle <christophe_remacle@hotmail.com>
2017-02-13 11:19:58 +0100

    Exercise part three

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,19 +6,31 @@
  * started at 13/02/2017
  */
 
- const fHandleTabClick = function( oEvent ) {
- 	oEvent.preventDefault();
- 	const $target = oEvent.currentTarget;
+let iTrombinoCurrentIndex = 0,
+ 	iTrombinoLength,
+ 	$trombinoFigures;
 
- 	if ( $target.parentNode.classList.contains( "active" ) ) {
- 		return;
- 	}
+const fHandleTabClick = function( oEvent ) {
+	oEvent.preventDefault();
+	const $target = oEvent.currentTarget;
 
- 	document.querySelector( "ul.nav-tabs li.active" ).classList.remove( "active" );
- 	$target.parentNode.classList.add( "active" );
- 	document.querySelector( ".tab-content .tab-pane.active" ).classList.remove( "active" );
- 	document.getElementById( $target.getAttribute( "data-tab-target" ) ).classList.add( "active" );
- };
+	if ( $target.parentNode.classList.contains( "active" ) ) {
+		return;
+	}
+
+	document.querySelector( "ul.nav-tabs li.active" ).classList.remove( "active" );
+	$target.parentNode.classList.add( "active" );
+	document.querySelector( ".tab-content .tab-pane.active" ).classList.remove( "active" );
+	document.getElementById( $target.getAttribute( "data-tab-target" ) ).classList.add( "active" );
+};
+
+const fUpdateTrombino = function() {
+	$trombinoFigures[ iTrombinoCurrentIndex ].classList.add( "hide" );
+	if ( ++iTrombinoCurrentIndex === iTrombinoLength ) {
+		iTrombinoCurrentIndex = 0;
+	}
+	$trombinoFigures[ iTrombinoCurrentIndex ].classList.remove("hide");
+};
 
 // When page load...
 window.addEventListener( "load", function() {
@@ -34,5 +46,13 @@ window.addEventListener( "load", function() {
 	Array.from( document.querySelectorAll( 'ul.nav.nav-tabs a' ) ).forEach( function( $elt ) {
 		$elt.addEventListener( "click", fHandleTabClick )
 	} );
+
+	// 3. trombino
+	document.querySelectorAll( "#trombino figure:not(:first-of-type)" ).forEach( function( $elt ) {
+		$elt.classList.add( "hide" );
+	} );
+	$trombinoFigures = Array.from( document.querySelectorAll( "#trombino figure" ) );
+	iTrombinoLength = $trombinoFigures.length;
+	setInterval( fUpdateTrombino, 1000 );
 
 } );
