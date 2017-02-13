02be3646750ef152a37eda4db95284a4c1634994
Christophe Remacle <christophe_remacle@hotmail.com>
2017-02-13 12:14:14 +0100

    Exercise one and two

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,3 +7,25 @@
  */
 
 // TODO: with jQuery
+
+let $tabs;
+
+const fChangeTab = function ( oEvent ) {
+	oEvent.preventDefault();
+
+	$tabs.parent().filter( ".active" ).removeClass( "active" );// filter est un filtre css donc il faut le . de la class
+	$( this ).parent().addClass( "active" );
+	$( ".tab-content .tab-pane.active" ).removeClass( "active" );
+	$( `#${ $( this ).data( "tab-target" ) }` ).addClass( "active" );
+};
+
+$( function() {
+
+	// 1. link with rel=external
+	$( 'a[rel*="external"]' ).attr( "target", "_new" );
+
+	// 2. tab
+	( $tabs = $( "ul.nav.nav-tabs a" ) ).on( "click", fChangeTab );
+
+
+} );
